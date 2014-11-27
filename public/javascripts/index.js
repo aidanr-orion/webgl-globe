0a7e5e61ad1a5991f9949f41169885d8f92ac6f0
Aidan Redpath <aidanr@ca.ibm.com>
2014-11-27 20:36:44 +0000

    Added better realtime capabilities

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,30 +11,12 @@ globe.addPoint(21, 78);
 // Mongolia
 globe.addPoint(46, 105);
 
-var xmlHttp = new XMLHttpRequest();
-xmlHttp.open("GET", "/data", false);
+var xmlHttp = null;
+
+xmlHttp = new XMLHttpRequest();
+xmlHttp.open( "GET", "/data", false );
 xmlHttp.send( null );
-var points = JSON.parse(xmlHttp.responseText);
 
-points.forEach(function(point) {
-  var date = new Date();
-  var hour = date.getHours();
-  var minute = date.getMinutes();
-  if(point.hr <= hour && point.min <= minute) return;
-  var millisTill10 = new Date(date.getFullYear(), date.getMonth(), date.getDate(), point.hr, point.min, 0, 0) - date;
-  timeoutPoint(point.lat, point.lng, millisTill10);
-});
+var json = JSON.parse(xmlHttp.responseText);
 
 
-function timeoutPoint(lat, lng, time){
- setTimeout(plotPoint(lat, lng), time);
-}
-function plotPoint (lat, lng){
-  return function(){
-    console.log("point: " + new Date());
-    globe.addPoint(lat, lng);
-  };
-}
-
-
-timeoutStuff(46, 105, 1000);
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,34 @@
 var express = require('express');
 var router = express.Router();
+
 var GAPI = require('gapitoken');
 var GA = require('googleanalytics');
 
-var token;
-
-// Get google token
+var client = process.env.OAUTH_CLIENT;
+var secret = process.env.OAUTH_SECRET;
+ 
 var gapi = new GAPI({
-  iss: "",
-  scope: "",
-  keyFile: ""
-}, function(err){
-  if(err) console.log(err);
-  gapi.getToken(function(err, t){
-    if(err) return console.log(err);
-    token = t;
-  });
+	iss : client,
+	scope : "https://www.googleapis.com/auth/analytics.readonly",
+	key : secret
+}, function(err) {
+	 gapi.getToken(function (err, token) {
+	 	if(err) return console.log(err);
+	 });
 });
 
 
+function parseOptions (req) {
+	var options = {
+		'ids': process.env.GOOGLE_PROFILE_ID,
+	    'start-date' : 'yesterday',
+	    'end-date' : 'yesterday',
+	    'dimensions' : 'ga:latitude, ga:longitude, ga:hour, ga:minute',
+	    'metrics' : ['ga:sessions'],
+	    'sort' : 'ga:hour, ga:minute'
+	};
+	return options;
+}
 
 
 
@@ -28,35 +38,29 @@ router.get('/', function(req, res) {
 });
 
 router.get('/data/', function(req, res) {
-  var config = {
-    "token": token
-  };
-  var ga = new GA.GA(config);
-
-  // Get the past day's data
-  var options = {
-    'ids': '',
-    'start-date' : 'yesterday',
-    'end-date' : 'yesterday',
-    'dimensions' : 'ga:latitude, ga:longitude, ga:hour, ga:minute',
-    'metrics' : ['ga:sessions'],
-    'sort' : 'ga:hour, ga:minute'
-  };
-  ga.get(options, function(err, entries) {
-    if (err) console.log(err);
-
-    var points = entries.map(function(val){
-      var dim = val.dimensions[0];
-      var point = {
-        lat: parseFloat(dim["ga:latitude"]),
-        lng: parseFloat(dim["ga:longitude"]),
-        hr: parseInt(dim["ga:hour"]),
-        min: parseInt(dim["ga:minute"])
-      }
-      return point;
-    });
-    res.json(points);
+  gapi.getToken(function (err, token){
+  	if (err) return res.json(err);
+  	var ga = new GA.GA({"token": token});
+  	var options = parseOptions(req);
+  	ga.get(options, function(err, entries){
+  		if (err) return res.json(err);
+  		
+  		 var points = entries.map(function(val){
+	 		 var dim = val.dimensions[0];
+	         var point = {
+		        lat: parseFloat(dim["ga:latitude"]),
+		        lng: parseFloat(dim["ga:longitude"]),
+		        hr: parseInt(dim["ga:hour"]),
+		        min: parseInt(dim["ga:minute"])
+		      };
+      		 return point;
+    	});
+  		
+  		res.json(points);
+  	});
   });
+
 });
 
 module.exports = router;
+
